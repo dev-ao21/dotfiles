dc9110dd62b407ec95b46ed27f6f338007647aa1
Abdo Omar <dev.abdo.omar@gmail.com>
2025-09-07 16:50:13 +0200

    wezterm: use default Jetbrains Mono nerd font

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -4,6 +4,5 @@ local config = wezterm.config_builder()
 config.enable_tab_bar = false
 config.color_scheme = 'GitHub Dark'
 config.font_size = 16.0
-config.font = wezterm.font("MesloLGS Nerd Font Mono")
 
 return config
